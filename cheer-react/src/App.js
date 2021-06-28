7fe6481c3c9e26a52908d5f5000a3d1d9bfe1d1d
Charlotte Qazi <charlottemdavies@hotmail.co.uk>
2021-06-28 16:37:14 +0100

    style update

--- cheer-react/src/App.js
+++ cheer-react/src/App.js
@@ -4,20 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Cheer Sequence Pose Machine</h1>
+        <div class='buttonContainer'>
+            <button type="button" onclick="init()" id='startButton'>Start</button>
+            <button type="button" onclick="pause()" id='stopButton'>Stop</button>
+        </div>
+        <div>
+            <canvas id="canvas"></canvas>
+        </div>
+        <div id="label-container"></div>
+
+    </main>
     </div>
   );
 }
